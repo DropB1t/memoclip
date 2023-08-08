ff06b5017fe710a44389205c3c369bf2f8d5f022
Yuriy Rymarchuk <yura.ita.com@gmail.com>
2023-08-08 18:53:48 +0200

    feature: polished scroll position at go back action

--- src/routes/(protected)/profile/[user]/+page.svelte
+++ src/routes/(protected)/profile/[user]/+page.svelte
@@ -7,13 +7,42 @@
 
 	export let data
 
-	let scroller: HTMLDivElement
+	let scrollPos: number
+	let loading = false
+	let can_restore = false
 
 	let next = data.next as string
 	let memos = data.memos as Memo[]
 
+	$: if ($navigating) {
+		can_restore = $navigating.type === 'popstate'
+	}
+
+	export const snapshot = {
+		capture: () => ({
+			memos,
+			next,
+			scrollPos
+		}),
+		restore: async (values) => {
+			if (!can_restore) return
+
+			memos = values.memos
+			next = values.next
+			if (values.scrollPos) {
+				restorePos(values.scrollPos)
+			}
+		}
+	}
+
+	async function restorePos(scrollPos: number) {
+		await tick()
+		window.scrollTo({ top: scrollPos, behavior: 'instant' })
+	}
+
 	async function fetchMemos() {
-		if (!next) return
+		if (loading || !next) return
+		loading = true
 
 		const response = await fetch(`/api/created?start=${next}`)
 		const result = await response.json()
@@ -27,45 +56,16 @@
 			// TODO Handle error better
 			/* throw new Error('Impossibile to retrieve information form the link') */
 		}
-	}
 
-	let can_restore = false
-
-	$: if ($navigating) {
-		can_restore = $navigating.type === 'popstate'
-	}
-
-	export const snapshot = {
-		capture: () => ({
-			memos,
-			next,
-			scrollPos: scroller.scrollTop
-		}),
-		restore: async (values) => {
-			if (!can_restore) return
-
-			memos = values.memos
-			next = values.next
-
-			if (values.scrollPos) {
-				restorePos(values.scrollPos)
-			}
-		}
-	}
-
-	async function restorePos(scrollRestored: number) {
-		await tick()
-		console.log('[restored] ' + scrollRestored)
-		scroller.scrollTo(0, scrollRestored)
+		loading = false
 	}
 </script>
 
+<svelte:window bind:scrollY={scrollPos} />
+
 <h1 class="text-2xl my-5 mt-12 text-center">Your Memos</h1>
 
-<div
-	bind:this={scroller}
-	class="flex flex-col md:flex-row md:flex-wrap gap-5 justify-center items-center px-2"
->
+<div class="flex flex-col md:flex-row md:flex-wrap gap-5 justify-center items-center px-2">
 	{#each memos as memo (memo.id)}
 		<div data-memo-id={memo.id} class="w-fit">
 			<MemoCard {memo} />
@@ -75,30 +75,20 @@
 	{/each}
 	<InfiniteScroll
 		window={true}
-		threshold={2 * 500}
+		threshold={500}
 		hasMore={next != null}
 		on:loadMore={() => {
 			fetchMemos()
 		}}
 	/>
 </div>
-<div class="mt-4 mx-auto text-center">
-	{#if next}
+
+<div class="mt-5 mx-auto text-center">
+	{#if loading}
+		<div class="my-auto text-center">
+			<span class="loading loading-dots text-secondary loading-lg" />
+		</div>
+	{:else if next}
 		<a class="link text-base-content" href="{$page.url.pathname}?start={next}">next page</a>
 	{/if}
 </div>
-
-<!-- <MemoList
-		bind:this={list}
-		endpoint="/api/created"
-		memos={data.memos}
-		next={data.next}
-		on:loaded={(e) => {
-			data.memos = [...data.memos, ...e.detail.memos]
-			data.next = e.detail.next
-			/* console.log(data.memos) */
-		}}
-	>
-		<h1 slot="header" class="text-2xl my-5 text-center ">Your Memos</h1>
-		<p slot="empty" class="text-center">No memos yet :c</p>
-	</MemoList> -->
