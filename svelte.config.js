a4a06e04b7a8573b3a411e56b323ffb424357f69
Yuriy Rymarchuk <yura.ita.com@gmail.com>
2023-08-29 19:55:40 +0200

    unregister SW in production

--- svelte.config.js
+++ svelte.config.js
@@ -17,7 +17,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		serviceWorker: {
-			register: true
+			register: false
 		}
 	}
 }
